2d400472499eecdd8f59ba9a10fe3fc558755bc3
Laurenceisdabest <93589056+Laurenceisdabest@users.noreply.github.com>
2022-01-04 11:16:47 +0000

    Started Documentation

---
+++ Untitled-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Using Echo</title>
+
+</head>
+<body>
+
+  <p>This is standard HTML</p>
+
+  <?php
+  echo 'This was generated using PHP!';
+  echo '<h1>PHP & HTML</h1><p>PHP can be used to generate
+      HTML.</p>';
+
+  ?>
+
+</body>
+</html>
